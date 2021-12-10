e4e09cb7eae8a3f15ba679a855667f08bf00f4b9
pollylilh <794211268@qq.com>
2021-12-10 10:55:32 +0800

    格式化

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,20 @@
 <template>
   <div>
-    <!-- <HelloWorld /> -->
-    <!-- <modal /> -->
-    <Suspense>
-      <template #default>
-        <!-- <AsyncShow /> -->
-        <girl-show />
-      </template>
-      <template #fallback>
-        <h1>Loading...</h1>
-      </template>
-    </Suspense>
   </div>
 </template>
 
 <script lang="ts">
-import { ref, onErrorCaptured } from "vue";
-// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-// import modal from "./components/Modal.vue";
-// import useUrlAxios from "./hooks/useUrlAxios";
-// import AsyncShow from "./components/AsyncShow.vue";
-import GirlShow from "./components/GirlShow.vue";
 
 export default {
   name: "App",
   components: {
-    // HelloWorld,
-    // modal,
-    // AsyncShow,
-    GirlShow,
   },
   setup() {
-    onErrorCaptured((error) => {
-      console.log(`error====>`, error);
-      return true;
-    });
+
     return {};
   },
 };
 </script>
 
 <style>
-#app {
-  text-align: center;
-  color: #2c3e50;
-}
 </style>
