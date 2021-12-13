f7fd0c41b57c2f8470aff3a850ce3723c564cc40
pollylilh <794211268@qq.com>
2021-12-13 16:49:43 +0800

    创建todoList分支

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,13 @@
 <template>
-  <h1>provide 与 inject: 实现跨层级组件(祖孙)间通信</h1>
-  <h2>父组件</h2>
-  <p>当前颜色: {{ color }}</p>
-  <button @click="color = 'red'">红</button>
-  <button @click="color = 'yellow'">黄</button>
-  <button @click="color = 'blue'">蓝</button>
-
-  <hr />
-  <Son />
+  <div></div>
 </template>
 
 <script lang="ts">
-import { provide, ref } from "vue";
-/* 
-- provide` 和 `inject` 提供依赖注入，功能类似 2.x 的 `provide/inject
-- 实现跨层级组件(祖孙)间通信
-*/
-
-import Son from "./components/Son.vue";
 export default {
   name: "App",
-  components: {
-    Son,
-  },
+  components: {},
   setup() {
-    const color = ref("red");
-    // 提供数据
-    provide("color", color);
-
-    return {
-      color,
-    };
+    return {};
   },
 };
 </script>
